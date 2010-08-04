6122e69712be507916b7b5bb039bd9e8c24fa8bd
Tommy Montgomery <tommy.mont@gmail.com>
2010-08-04 21:50:46 +0000

    made DefaultEqualityComparer use Util::compare()

--- src/Phinq/DefaultEqualityComparer.php
+++ src/Phinq/DefaultEqualityComparer.php
@@ -17,21 +17,7 @@
 		}
 
 		public function equals($a, $b) {
-			if (is_int($a) || is_float($a)) {
-				return (is_int($b) || is_float($b)) && $a == $b ? 0 : ($a < $b ? -1 : 1);
-			} else if ($a === null) {
-				return $b === null ? 0 : ($a < $b ? -1 : 1);
-			} else if (is_string($a)) {
-				return is_string($b) && $a === $b ? 0 : ($a < $b ? -1 : 1);
-			} else if (is_object($a)) {
-				return is_object($b) && $a === $b ? 0 : 1; //reference equals
-			} else if (is_array($a)) {
-				return is_array($b) && $a === $b ? 0 : ($a < $b ? -1 : 1); //keys must match
-			} else if (is_resource($a)) {
-				return is_resource($b) && $a === $b ? 0 : ($a < $b ? -1 : 1);
-			} else {
-				return $a === $b ? 0 : ($a < $b ? -1 : 1);
-			}
+			return Util::compare($a, $b);
 		}
 	}
 	
